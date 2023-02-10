21258cc71dcbbc9d2bfd03b4e19dd6b1565aedcc
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-02-10 15:03:10 +0100

    creato migrates per cambiare tipo dato collona, creato seeder per inserire dati (fake) all'interno delle colonne della tabella

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +14,27 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
+        for ($i=0; $i < 100 ; $i++) { 
+            # code...
+
+            $newTrain = new Train();
+            $newTrain->azienda = $faker->word();
+            $newTrain->stazione_di_partenza = $faker->word();
+            $newTrain->stazione_di_arrivo = $faker->word();
+            $newTrain->orario_di_partenza = $faker->dateTimeBetween('-1 week', '+1 week');
+            $newTrain->orario_di_arrivo = $faker->dateTimeBetween('-1 week', '+1 week');
+            $newTrain->codice_treno = $faker->randomNumber(4, true);
+            $newTrain->numero_carrozze = $faker->randomDigitNot(0);
+            $newTrain->in_orario = $faker->boolean();
+            $newTrain->cancellato = $faker->boolean();
+            $newTrain->save();
+            
+
+
+
+        }
     }
 }
