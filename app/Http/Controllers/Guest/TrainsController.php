eca2dc31aeb721625ebd3eae61a5e966e324ef54
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-02-10 15:28:22 +0100

    presi dati che serviva (treni disponibili nella gionata odierna) dal db e stampati a schermo

--- app/Http/Controllers/Guest/TrainsController.php
+++ app/Http/Controllers/Guest/TrainsController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainsController extends Controller
 {
     public function index(){
 
+        $trains = Train::where("orario_di_partenza", ">=", "2023-02-10 00:00:00")->where("orario_di_partenza", "<", "2023-02-11 00:00:00")->get();
 
-        
-        return view("home");
+        return view("home", compact("trains"));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,10 +17,47 @@
 
 <body>
 
+    <main>
 
-    
+        <div class="container">
+            <div class="row">
 
-    
+                @foreach($trains as $train)
+
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Azienda: {{$train->azienda}} </h5>
+                            <p class="card-text">Stazione di partenza: {{$train->stazione_di_partenza}}. stazione di arrivo: {{$train->stazione_di_arrivo}}. </p>
+                            <p class="card-text m-0">orario di partenza: {{$train->orario_di_partenza}}.</p>
+                            <p class="card-text">orario di arrivo: {{$train->orario_di_arrivo}}.</p>
+                            <p class="card-text">numero carrozze: {{$train->numero_carrozze}}</p>
+                            <p class="card-text m-0">
+                                @if ($train->in_orario == 0)
+                                treno in orario.
+                                @else
+                                treno in ritardo.
+                                @endif
+                            </p>
+                            <p class="card-text">
+                                @if ($train->cancellato == 0)
+                                Stato treno: ok.
+                                @else
+                                Stato Treno: treno soppresso.
+                                @endif
+                            </p>
+                            <a href="#" class="btn btn-primary">Acquista Biglietto</a>
+                        </div>
+                    </div>
+                </div>
+
+                @endforeach
+
+            </div>
+        </div>
+
+    </main>
+
 </body>
 
 </html>
